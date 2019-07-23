65d53c4c8bef52f2571ed75066a31aa1a742b829
Phesto Mwakyusa <phesto@qusaz.com>
2019-07-24 01:22:07 +0300

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'QUSAZ Intelligent Solutions',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'QUSAZ Intelligent Solutions Ltd',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
